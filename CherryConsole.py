9a817279ea0974b9d44d9613a1056b4e45f731c5
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2020-12-10 16:07:20 +0300

    2.1
    added vk short url checker and castomize start logo

--- CherryConsole.py
+++ CherryConsole.py
@@ -1,47 +1,61 @@
 ﻿from jinja2 import Template
 from gen_bot import render_bot
 import time
+import requests as re
 
-print("████████████████████████████████████         __.--~~.,-.__     ")
-print("█    ██ ██ ██   ██    ██    ██  █  █         `~-._.-(`-.__`-.  ")
-print("█ ██ ██ ██ ██ ████ ██ ██ ██ ███   ██                 \    `~~` ")
-print("█ █████    ██   ██    ██    ████ ███             .--./ \       ")
-print("█ ██ ██ ██ ██ ████ █ ███ █ █████ ███            /#   \  \.--.  ")
-print("█    ██ ██ ██   ██ ██ ██ ██ ████ ███            \    /  /#   \ ")
-print("████████████████████████████████████             '--'   \    / ")
-print("███████████████████████████████████████                  '--'  ")
+print(" ████████████████████████████████████  __.--~~.,-.__     ")
+print(" █    ██ ██ ██   ██    ██    ██  █  █  `~-._.-(`-.__`-.  ")
+print(" █ ██ ██ ██ ██ ████ ██ ██ ██ ███   ██          \    `~~` ")
+print(" █ █████    ██   ██    ██    ████ ███      .--./ \       ")
+print(" █ ██ ██ ██ ██ ████ █ ███ █ █████ ███     /#   \  \.--.  ")
+print(" █    ██ ██ ██   ██ ██ ██ ██ ████ ███     \    /  /#   \ ")
+print(" ████████████████████████████████████      '--'   \    / ")
+print("███████████████████████████████████████            '--'  ")
 print("█    █    ██    ██    █    █ ████    ██")
 print("█ ██ █ ██ ██ ██ ██ ████ ██ █ ████ █████")
 print("█ ████ ██ ██ ██ ██    █ ██ █ ████    ██")
 print("█ ██ █ ██ ██ ██ █████ █ ██ █ ████ █████")
 print("█    █    ██ ██ ██    █    █    █    ██")
 print("███████████████████████████████████████")
-print('Powered by Sp0ge and MaBoiexit in 2017')
+print('Powered by Sp0ge and MaBoi')
+print('https://github.com/Sp0ge/CherryConsole')
 
 time.sleep(1)
 while True:
-    bot = False
+    menu = False
     a = 'stop'
     print('print "help" to get command list')
-    while bot == False:
+    while menu == False:
         print('')
-        a = input('$CherryConsole$->').replace(" ","")
+        a = input('$CherryConsole$ ->').replace(" ","")
         print('')
 
         if a == 'help':
             print('bc - start bot editor')
+            print('uc - url check')
             print('exit - stop program')
+            
 
         if a == 'exit':
             print('Powered by Sp0ge and MaBoiexit in 2017')
             time.sleep(1)
             break
+        
+        if a == 'bot editor' or a == 'bc' or a == "uc" or a == "urlcheck":
+            menu = True
             
-
-        if a == 'bot editor' or a == 'bc':
-            bot = True
-
-    if bot == True:
+    if a == "uc" or a == "urlcheck":
+                urls = input("vk short urls -> ").replace("#","").replace(",","").split(' ')
+                num = len(urls)
+                n = 0
+                print(' ')
+                while num > n:
+                    url = 'https://vk.com/' + urls[n]
+                    ans = re.get(url)
+                    print(urls[n], " - ", ans)      
+                    n += 1
+                
+    if a == 'bot editor' or a == 'bc':
             
         print("_________")
             
