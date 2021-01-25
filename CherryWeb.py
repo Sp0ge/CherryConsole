ffe83dd9487c28b20dcd22b44f3f8d427a929ff5
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2021-01-25 22:18:46 +0300

    1.0
    developing web form for web interface

--- CherryWeb.py
+++ CherryWeb.py
@@ -1,27 +1,24 @@
-from flask import Flask, request, render_template, send_file
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 @app.route('/')
-@app.route('/index.html', methods=['GET', 'POST'])
-def form():
-    try:
-        a = form.objects.get()
-    except form.DoesNotExist:
-        a = None
-
-    if request.method == 'POST' and form.validate():
-        form = form(request.form)
-        user = User(form.name.data, form.token.data, form.qnum.data)
-        if form.is_valid():
-            form.save()
-            flash(user)
-
-    return render_template('index.html', {'form': form})
-
-
+@app.route('/index.html', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        url = request.form.get('url')
+        n = 0
+        rew = ''
+        urls = url.replace("  "," ").replace("#","").replace(",","").split(' ')
+        num = len(urls)
+        while num > n:
+            url = 'https://vk.com/' + urls[n]
+            ans = request.get(url)
+            answer = ("  ",'{} {}'.format(urls[n].ljust(30, ' '), ans),)
+            rew = rew + ' ' + answer
+            n += 1
+        message = rew
+    return render_template('index.html', message=message)
 
 if __name__ == "__main__":
     app.run()
